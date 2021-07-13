23a645683f79d1f4da8f4f4864d7853b9ecefe2a
Dominika Rycerz <dominikarycerz@hotmail.com>
2021-07-13 15:16:41 +0200

    Enabled changing size of the pen

--- index.js
+++ index.js
@@ -1,24 +1,56 @@
-var container = document.querySelector(".container");
-var item;
+// Function that creates 16x16 grid
 
-// Create 16x16 grid
-let createItem = () => {
-  for (var i = 0; i < 256; i++) {
-    item = document.createElement('div');
-    container.appendChild(item);
+// let createInitialBoard = () => {
+//   for (var i = 0; i < 256; i++) {
+//     let item = document.createElement('div');
+//     document.querySelector('.container').appendChild(item);
+//     item.classList.add('item');
+//     item.addEventListener('mouseover', changeColor);
+//   }
+// }
+// createInitialBoard();
+
+
+var container = document.querySelector('.container');
+var userInput = 50;
+
+
+let createInitialBoard = () => {
+
+  let gridSquare = userInput * userInput;
+  container.style.gridTemplateColumns = `repeat(${userInput}, 3fr)`;
+  container.style.gridTemplateRows = `repeat(${userInput}, 3fr)`;
+
+  for (let i = 1; i <= gridSquare; i++) {
+    let item = document.createElement('div');
+    container.insertAdjacentElement('afterbegin', item)
     item.classList.add('item');
-    item.addEventListener("mouseover", changeColor);
-
-    // Add event listener
-    // document.querySelector(".item").addEventListener("click", changeColor);
+    item.addEventListener('mouseover', changeColor);
   }
 }
-createItem();
+createInitialBoard();
 
-// Function that changes color of hovered item
+
+
+// Function that changes color of hovered item by selecting clicked item and adding a new class
 function changeColor() {
-  // Select clicked Item
-  var clickedItem = event.target;
-  // Add a new class to the clicked item
+  let clickedItem = event.target;
   clickedItem.classList.add('hovered');
+}
+
+
+// Function that makes a new grid
+document.querySelector('.reset-btn').addEventListener('click', createNewBoard);
+
+function createNewBoard() {
+  let allItems = container.querySelectorAll('div');
+
+  allItems.forEach(severalItem =>
+    severalItem.classList.remove('hovered')
+  );
+
+  userInput = prompt("Enter the number of squares per side for the new grid (max 100).");
+  userInput > 100 ? userInput = 100 : i = userInput;
+
+  createInitialBoard(userInput);
 }
